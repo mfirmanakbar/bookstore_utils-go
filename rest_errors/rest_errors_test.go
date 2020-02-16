8536948d1de64e2529232470a8271256e243775f
mfirmanakbar <em.firman.akbar@gmail.com>
2020-02-16 14:25:14 +0700

    inital version

---
+++ rest_errors/rest_errors_test.go
@@ -0,0 +1,41 @@
+package rest_errors
+
+import (
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"testing"
+)
+
+func TestNewInternalServerError(t *testing.T) {
+	err := NewInternalServerError("this is the expected message", errors.New("database error"))
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusInternalServerError, err.Status)
+	assert.EqualValues(t, "this is the expected message", err.Message)
+	assert.EqualValues(t, "internal_server_error", err.Error)
+	assert.NotNil(t, err.Causes)
+	assert.EqualValues(t, 1, len(err.Causes))
+	assert.EqualValues(t, "database error", err.Causes[0])
+}
+
+func TestNewBadRequestError(t *testing.T) {
+	err := NewBadRequestError("this is the expected message")
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusBadRequest, err.Status)
+	assert.EqualValues(t, "this is the expected message", err.Message)
+	assert.EqualValues(t, "bad_request", err.Error)
+}
+
+func TestNewNotFoundError(t *testing.T) {
+	err := NewNotFoundError("this is the expected message")
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusNotFound, err.Status)
+	assert.EqualValues(t, "this is the expected message", err.Message)
+	assert.EqualValues(t, "not_found", err.Error)
+}
+
+func TestNewError(t *testing.T) {
+	err := NewError("this is the expected message")
+	assert.NotNil(t, err)
+	assert.EqualValues(t, "this is the expected message", err.Error())
+}
